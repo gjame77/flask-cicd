54d0ef27078e4678fbdda6bd1f45a7c3e4229dd0
gjame77 <kangjaeku@naver.com>
2025-11-25 20:50:54 +0900

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/flask-app
 pip3 install -r requirements.txt
-nohup python3 /home/ec2-user/flask-cicd/app.py > app.log 2>&1 &
+nohup python3 /home/ec2-user/flask-app/app.py > app.log 2>&1 &
