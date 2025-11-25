5c4872c68dd48eaee259d0fbf60e4bff0d5647a0
gjame77 <kangjaeku@naver.com>
2025-11-25 14:01:27 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>AWS CI/CD Demo</h1><p>배포 완료</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
