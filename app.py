fa9fda3242bc260587aa72f57b6db5fca5787bcf
gjame77 <kangjaeku@naver.com>
2025-11-25 20:51:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>AWS CI/CD Demo</h1><p>배포 완료2</p>"
+    return "<h1>AWS CI/CD Demo</h1><p>배포 완료</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
